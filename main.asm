eab9e95270fb0f331ef8e3660226dc3998be2da4
UnaLuz <luz.cucarella97@gmail.com>
2021-05-25 13:07:44 -0300

    Funcion que incerta un 'barco' en la posicion indicada, con tamaño variable vertical u horizontal

--- main.asm
+++ main.asm
@@ -2,13 +2,122 @@
 .model small
 .stack 100h
 .data
+  posicion db "A4"
+  tablero db "  0 1 2 3 4 5 6 7 8 9", 0dh, 0ah
+          db " +-------------------", 0dh, 0ah
+          db "A|                   ", 0dh, 0ah
+          db "B|                   ", 0dh, 0ah
+          db "C|                   ", 0dh, 0ah
+          db "D|                   ", 0dh, 0ah
+          db "E|                   ", 0dh, 0ah
+          db "F|                   ", 0dh, 0ah
+          db "G|                   ", 0dh, 0ah
+          db "H|                   ", 0dh, 0ah
+          db "I|                   ", 0dh, 0ah
+          db "J|                   ", 0dh, 0ah, 24h
+
+  ; tableroXL db "x-------------------------------------------x", 0dh, 0ah
+  ;           db "|   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |", 0dh, 0ah
+  ;           db "|-------------------------------------------|", 0dh, 0ah
+  ;           db "| A |   |   |   |   |   |   |   |   |   |   |", 0dh, 0ah
+  ;           db "|-------------------------------------------|", 0dh, 0ah
+  ;           db "| B |   |   |   |   |   |   |   |   |   |   |", 0dh, 0ah
+  ;           db "|-------------------------------------------|", 0dh, 0ah
+  ;           db "| C |   |   |   |   |   |   |   |   |   |   |", 0dh, 0ah
+  ;           db "|-------------------------------------------|", 0dh, 0ah
+  ;           db "| D |   |   |   |   |   |   |   |   |   |   |", 0dh, 0ah
+  ;           db "|-------------------------------------------|", 0dh, 0ah
+  ;           db "| E |   |   |   |   |   |   |   |   |   |   |", 0dh, 0ah
+  ;           db "|-------------------------------------------|", 0dh, 0ah
+  ;           db "| F |   |   |   |   |   |   |   |   |   |   |", 0dh, 0ah
+  ;           db "|-------------------------------------------|", 0dh, 0ah
+  ;           db "| G |   |   |   |   |   |   |   |   |   |   |", 0dh, 0ah
+  ;           db "|-------------------------------------------|", 0dh, 0ah
+  ;           db "| H |   |   |   |   |   |   |   |   |   |   |", 0dh, 0ah
+  ;           db "|-------------------------------------------|", 0dh, 0ah
+  ;           db "| I |   |   |   |   |   |   |   |   |   |   |", 0dh, 0ah
+  ;           db "|-------------------------------------------|", 0dh, 0ah
+  ;           db "| J |   |   |   |   |   |   |   |   |   |   |", 0dh, 0ah
+  ;           db "x-------------------------------------------x", 0dh, 0ah, 24h
 
 .code
   main proc
     mov ax, @data
     mov ds, ax
 
+    xor ax, ax
+
+    mov bx, offset tablero
+
+    ;Poner un circulo en la posicion A4: x + y.cols -> 4 + 1.10 = 14 -> sería la posicion/indice del arreglo si consideramos solo 10 columnas
+    mov di, 2 ; Me posiciono en la columna del 0
+    add di, 8 ; Añado 2 por cada posicion que me quiero mover en X por los espacios agregados
+    mov al, 2 ; Me posiciono en la fila A
+    add al, 0 ; Sumo la cantidad de filas que me quiero mover
+    mov cl, 23 ; Pongo en cl la cantidad de columnas que hay (hay que contar todos los caracteres, incluso los saltos)
+    mul cl ; multiplico AL por CL
+    add di, ax ; Sumo a DI (la posicion en X) lo que tengo en AL (la posicion en y por la cant de columnas)
+
+    mov ax, 0 ; poner el barco en forma horizontal
+    mov cx, 3
+    call ponerBarco
+
+    mov di, 52 ; pos posiciones antes que el anterior (en teoria)
+    mov ax, 1 ; poner el barco en forma horizontal
+    mov cx, 4
+    call ponerBarco
+
+    ; Imprimir tablero
+    mov ah, 9
+    mov dx, bx
+    int 21h
+
     mov ax, 4c00h
     int 21h
   main endp
+
+  ; Recibe en BX el offset del tablero
+  ; en DI la coordenada en X + la coordenada en Y multiplicada por la cantidad de columnas (la posicion correspondiente)
+  ; en AX si será horizontal o vertical (0 horizontal o distinto de 0 vertical)
+  ; y en CX el tamaño del barco a colocar
+  ponerBarco proc
+  ;Cuido el entorno
+  push ax
+  push bx   ; El offset del tablero no me interesa modificarlo asi que por las dudas lo guardo, para no romper nada
+  push cx
+  push dx
+  push ax
+  push di
+  pushf
+  ; xor ax, ax
+  ; xor bx, bx
+  ; xor cx, cx
+  xor si, si
+  ; xor di, di
+
+  barco:
+  mov byte ptr [bx + di], "*" ; Pongo un simbolo  en la posicion DI del tablero
+
+  cmp ax, 0
+  je horizontal
+
+  ; AX no es cero por lo que coloco el barco de forma vertical
+  add di, 23 ; Le sumo 23 lugares porque hay 23 columnas y quiero la misma posicion en X pero 1 mas en Y
+  jmp continuarLoop
+
+  horizontal:
+  add di, 2 ; Me muevo 2 en el tablero para ignorar los espacios entre las columnas
+
+  continuarLoop:
+  loop barco
+
+  popf
+  pop di
+  pop si
+  pop dx
+  pop cx
+  pop bx
+  pop ax
+  ret
+  ponerBarco endp
 end main
