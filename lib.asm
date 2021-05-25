579ffdad79aa3ef06f1f5dd91395c9143f5a557a
UnaLuz <luz.cucarella97@gmail.com>
2021-05-25 19:18:36 -0300

    Agregada funcion que convierte coordenadas en ascii a indice

--- lib.asm
+++ lib.asm
@@ -6,12 +6,12 @@
 .code
   ;Declaracion de funciones publicas
   public ponerBarco
-
-
+  public obtenerIndice
   ; Recibe en BX el offset del tablero
   ; en DI la coordenada en X + la coordenada en Y multiplicada por la cantidad de columnas (la posicion correspondiente)
-  ; en AX si será horizontal o vertical (0 horizontal o distinto de 0 vertical)
-  ; en DL la cantidad total de caracteres por fila, en DH el ancho de cada columna
+  ; en SI si será horizontal o vertical (0 horizontal o distinto de 0 vertical)
+  ; en DX la cantidad de caracteres por fila (cantidad de columnas) si AX = 0, o tamaño de las columnas si AX != 0
+  ; en AL el caracter para representar el barco
   ; y en CX el tamaño del barco a colocar
   ponerBarco proc
   ;Cuido el entorno
@@ -19,20 +19,20 @@
   push bx   ; El offset del tablero no me interesa modificarlo asi que por las dudas lo guardo, para no romper nada
   push cx
   push dx
-  push ax
+  push si
   push di
   pushf
   ; xor ax, ax
   ; xor bx, bx
   ; xor cx, cx
   ; xor dx, dx
-  xor si, si
+  ; xor si, si
   ; xor di, di
 
   barco:
-  mov byte ptr [bx + di], "*" ; Pongo un simbolo  en la posicion DI del tablero
+  mov byte ptr [bx + di], al ; Pongo un simbolo  en la posicion DI del tablero
 
-  cmp ax, 0
+  cmp si, 0
   je horizontal
 
   ; AX no es cero por lo que coloco el barco de forma vertical
@@ -54,5 +54,55 @@
   pop ax
   ret
   ponerBarco endp
+
+
+  ; Recibo las coordenadas por DX (ej: DX = "B6")
+  ; Recibo el ancho de las columnas por CL
+  ; Recibo el largo de las filas por CH
+  obtenerIndice proc
+  ;Cuido el entorno
+  push ax
+  push bx
+  push cx
+  push dx
+  push si
+  ; push di
+  pushf
+  xor ax, ax
+  xor bx, bx
+  ; xor cx, cx
+  ; xor dx, dx
+  xor si, si
+  xor di, di
+
+  ;Poner un circulo en la posicion A4: x + y.cols -> 4 + 1.10 = 14 -> sería la posicion/indice del arreglo si consideramos solo 10 columnas
+  ;Calculo la columna
+  mov di, 6 ; Me posiciono en la columna del 0
+  mov al, dl ; Pongo la coordenada de la columna (el numero) en AL
+  sub al, 30h ; Paso el caracter a numero
+  
+  mul cl ; multiplico AL por CL
+  add di, ax ; Me posiciono en el índice correspondiente
+
+  xor ax, ax
+  ;Sumo las filas a la columna
+  mov al, 2 ; Me posiciono en la fila A
+
+  add al, dh ; Pongo el caracter de la fila en AL
+  sub al, 41h ; Paso la letra ascii a numero (asumiendo que es una letra mayuscula)
+
+  mul ch ; multiplico AL por Ch (por el largo de las filas)
+  add di, ax ; Sumo a DI (la posicion en X) lo que tengo en AL (la posicion en y por la cant de columnas)
+
+  popf
+  ; pop di ; Devuelvo el indice correspondiente por DI
+  pop si
+  pop dx
+  pop cx
+  pop bx
+  pop ax
+  ret
+
+  obtenerIndice endp
 
 end
--- main.asm
+++ main.asm
@@ -2,68 +2,174 @@
 .model small
 .stack 100h
 .data
-  tablero db "  0 1 2 3 4 5 6 7 8 9 ", 0dh, 0ah
-          db " +-------------------+", 0dh, 0ah
-          db "A|                   |", 0dh, 0ah
-          db "B|                   |", 0dh, 0ah
-          db "C|                   |", 0dh, 0ah
-          db "D|                   |", 0dh, 0ah
-          db "E|                   |", 0dh, 0ah
-          db "F|                   |", 0dh, 0ah
-          db "G|                   |", 0dh, 0ah
-          db "H|                   |", 0dh, 0ah
-          db "I|                   |", 0dh, 0ah
-          db "J|                   |", 0dh, 0ah
-          db " +-------------------+", 24h
+; Porta-aviones PPPPP
+; Nave de batalla BBBB
+; Crucero de batalla CCC
+; Submarino BBB
+; Destructor DD
+
+  posicion db "A4", 24h
+
+  msj db "Ingrese Y para continuar, otra tecla para salir", 0dh, 0ah, 24h
+
+  impTablero db "El tablero:", 0dh, 0ah, 0dh, 0ah
+  ; tablero db "    0 1 2 3 4 5 6 7 8 9 ", 0dh, 0ah
+  ;         db "   +-------------------+", 0dh, 0ah
+  ;         db "  A|                   |", 0dh, 0ah
+  ;         db "  B|                   |", 0dh, 0ah
+  ;         db "  C|                   |", 0dh, 0ah
+  ;         db "  D|                   |", 0dh, 0ah
+  ;         db "  E|                   |", 0dh, 0ah
+  ;         db "  F|                   |", 0dh, 0ah
+  ;         db "  G|                   |", 0dh, 0ah
+  ;         db "  H|                   |", 0dh, 0ah
+  ;         db "  I|                   |", 0dh, 0ah
+  ;         db "  J|                   |", 0dh, 0ah
+  ;         db "   +-------------------+", 0dh, 0ah, 24h
+
+  tableroXL db "x-------------------------------------------x ", 0dh, 0ah
+            db "|   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | ", 0dh, 0ah
+            db "| A |   |   |   |   |   |   |   |   |   |   | ", 0dh, 0ah
+            db "| B |   |   |   |   |   |   |   |   |   |   | ", 0dh, 0ah
+            db "| C |   |   |   |   |   |   |   |   |   |   | ", 0dh, 0ah
+            db "| D |   |   |   |   |   |   |   |   |   |   | ", 0dh, 0ah
+            db "| E |   |   |   |   |   |   |   |   |   |   | ", 0dh, 0ah
+            db "| F |   |   |   |   |   |   |   |   |   |   | ", 0dh, 0ah
+            db "| G |   |   |   |   |   |   |   |   |   |   | ", 0dh, 0ah
+            db "| H |   |   |   |   |   |   |   |   |   |   | ", 0dh, 0ah
+            db "| I |   |   |   |   |   |   |   |   |   |   | ", 0dh, 0ah
+            db "| J |   |   |   |   |   |   |   |   |   |   | ", 0dh, 0ah
+            db "x-------------------------------------------x ", 0dh, 0ah, 24h
+  cols db 48 ;26
+  rows db 12
+  colW db 4 ;2
 
 .code
 ; Importo funciones de la libreria
 extrn ponerBarco:proc
+extrn obtenerIndice:proc
 
   main proc
     mov ax, @data
     mov ds, ax
 
+inicio:
+    call Clearscreen
+
     xor ax, ax
+    xor bx, bx
+    xor cx, cx
+    xor dx, dx
+    xor si, si
+    xor di, di
+    
+    mov bx, offset tableroXL
 
-    mov bx, offset tablero
+    ; ;Poner un circulo en la posicion A4: x + y.cols -> 4 + 1.10 = 14 -> sería la posicion/indice del arreglo si consideramos solo 10 columnas
+    ; mov di, 2 ; Me posiciono en la columna del 0
+    ; add di, 12 ; Añado 4 por cada posicion que me quiero mover en X por los espacios agregados
+    ; mov al, 2 ; Me posiciono en la fila A
+    ; add al, 0 ; Sumo la cantidad de filas que me quiero mover
+    ; mov cl, cols ; Pongo en cl la cantidad de columnas que hay (hay que contar todos los caracteres, incluso los saltos)
+    ; mul cl ; multiplico AL por CL
+    ; add di, ax ; Sumo a DI (la posicion en X) lo que tengo en AL (la posicion en y por la cant de columnas)
 
-    ;Poner un circulo en la posicion A4: x + y.cols -> 4 + 1.10 = 14 -> sería la posicion/indice del arreglo si consideramos solo 10 columnas
-    mov di, 2 ; Me posiciono en la columna del 0
-    add di, 8 ; Añado 2 por cada posicion que me quiero mover en X por los espacios agregados
-    mov al, 2 ; Me posiciono en la fila A
-    add al, 0 ; Sumo la cantidad de filas que me quiero mover
-    mov cl, 24 ; Pongo en cl la cantidad de columnas que hay (hay que contar todos los caracteres, incluso los saltos)
-    mul cl ; multiplico AL por CL
-    add di, ax ; Sumo a DI (la posicion en X) lo que tengo en AL (la posicion en y por la cant de columnas)
 
-    mov ax, 0 ; poner el barco en forma horizontal
-    mov cx, 3 ; Barco de tamaño 3
-    mov dx, 2
+    mov dx, "D3"
+    mov cl, colW
+    mov ch, cols
+    call obtenerIndice
+    ; Porta aviones - PPPPP
+    mov si, 0 ; poner el barco en forma horizontal
+    mov al, "P" ; Caracter que representa al barco
+    mov cx, 5 ; Barco de tamaño 3
+    mov dx, 0
+    add dl, colW ; paso el ancho de cada columna
     call ponerBarco
 
-    mov di, 2 ; Me posiciono en la columna del 0
-    add di, 4 ; Añado 2 por cada posicion que me quiero mover en X por los espacios agregados
-    mov al, 2 ; Me posiciono en la fila A
-    add al, 0 ; Sumo la cantidad de filas que me quiero mover
-    mov cl, 24 ; Pongo en cl la cantidad de columnas que hay (hay que contar todos los caracteres, incluso los saltos)
-    mul cl ; multiplico AL por CL
-    add di, ax ; Sumo a DI (la posicion en X) lo que tengo en AL (la posicion en y por la cant de columnas)
-    ; pos posiciones antes que el anterior (en teoria)
+    ; mov di, 2 ; Me posiciono en la columna del 0
+    ; add di, 4 ; Añado 2 por cada posicion que me quiero mover en X por los espacios agregados
+    ; mov al, 2 ; Me posiciono en la fila A
+    ; add al, 0 ; Sumo la cantidad de filas que me quiero mover
+    ; mov cl, cols ; Pongo en cl la cantidad de columnas que hay (hay que contar todos los caracteres, incluso los saltos)
+    ; mul cl ; multiplico AL por CL
+    ; add di, ax ; Sumo a DI (la posicion en X) lo que tengo en AL (la posicion en y por la cant de columnas)
+    ; ; pos posiciones antes que el anterior (en teoria)
 
-    mov ax, 1 ; poner el barco en forma vertical
+    mov dx, "A9"
+    mov cl, colW
+    mov ch, cols
+    call obtenerIndice
+
+    ;Nave de batalla BBBB
+    mov si, 1 ; poner el barco en forma vertical
+    mov al, "B" ; Caracter que representa al barco
     mov cx, 4 ; barco de tamaño 4
-    mov dx, 24 ; cantidad de caracteres por columna
+    mov dx, 0
+    add dl, cols ; cantidad de caracteres por columna
     call ponerBarco
+
+
+    ; Destructor DD
+    mov dx, "E5"
+    mov cl, colW
+    mov ch, cols
+    call obtenerIndice
+
+    ;Nave de batalla BBBB
+    mov si, 1 ; poner el barco en forma vertical
+    mov al, "D" ; Caracter que representa al barco
+    mov cx, 2 ; barco de tamaño 4
+    mov dx, 0
+    add dl, cols ; cantidad de caracteres por columna
+    call ponerBarco
+
 
     ; Imprimir tablero
     mov ah, 9
-    mov dx, bx
+    mov dx, offset tableroXL
     int 21h
+
+    mov ah, 9
+    mov dx, offset msj
+    int 21h
+
+    mov ah, 1
+    int 21h
+
+    cmp al, "y"
+    jne fin
+
+    jmp inicio
+
+    fin:
 
     mov ax, 4c00h
     int 21h
   main endp
+
+
+
+  Clearscreen proc
+  push ax
+  push es
+  push cx
+  push di
+  mov ax,3
+  int 10h
+  mov ax,0b800h
+  mov es,ax
+  mov cx,1000
+  mov ax,7
+  mov di,ax
+  cld
+  rep stosw
+  pop di
+  pop cx
+  pop es
+  pop ax
+  ret 
+Clearscreen endp
 
   
 end main
